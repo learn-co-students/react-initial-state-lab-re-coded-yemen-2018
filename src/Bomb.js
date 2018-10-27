215d443a0da9d7b3621ceee082bdbaed4bc7b083
sondos alansi <sondos.alansi@yahoo.com>
2018-10-27 20:49:24 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,22 @@
+// your ImageSlider code here!
+import React from 'react';
+
+export default class Bomb  extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    secondsLeft:props.initialCount
+
+    }
+  }
+
+  render() {
+    return (
+      <p>
+          {this.state.secondsLeft===0? "Boom!":`${this.state.secondsLeft} seconds left before I go boom!`}
+      </p>
+    )
+  }
+}
 // your Bomb code here!
